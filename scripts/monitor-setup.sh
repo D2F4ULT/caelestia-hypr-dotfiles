5793786875e93561b7db36304dbb44ff7ea79a7d
andrew_arch_main <andrew@arch.com>
2026-07-15 21:12:53 +0200

    Changed keybinds a bit and monitor set up -> Alt+z picet movement

--- scripts/monitor-setup.sh
+++ scripts/monitor-setup.sh
@@ -3,20 +3,20 @@
 MONITORS=$(hyprctl monitors -j)
 
 if echo "$MONITORS" | grep -q "Chimei Innolux Corporation 0x15F5"; then
-	echo "Laptop setup detected"
+    echo "Laptop setup detected"
 
-	hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1.5"
+    hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1.5"
 
 elif echo "$MONITORS" | grep -q "DELL"; then
-	echo "Desk setup detected"
+    echo "Desk setup detected"
 
-	hyprctl keyword monitor "HDMI-A-1,2560x1440@59.95,0x0,1"
-	hyprctl keyword monitor "DP-2,3840x2160@60,2560x0,1"
+    hyprctl keyword monitor "HDMI-A-1,2560x1440@59.95,0x0,1"
+    hyprctl keyword monitor "DP-2,3840x2160@60,2560x0,1"
 
 else
-	echo "Unknown monitor detected"
+    echo "Unknown monitor detected"
 
-	for monitor in $(hyprctl monitors -j | jq -r '.[].name'); do
-		hyprctl keyword monitor "$monitor,preferred,auto,1"
-	done
+    for monitor in $(hyprctl monitors -j | jq -r '.[].name'); do
+        hyprctl keyword monitor "$monitor,preferred,auto,1"
+    done
 fi
